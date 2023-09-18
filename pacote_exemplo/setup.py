659f426fb71aeb5889efbf14a343fab413120adf
FidyBack <abel.cavalc@gmail.com>
2023-09-18 03:36:19 -0300

    setup.py

---
+++ pacote_exemplo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='dev_aberto_abel',
+    version='0.1',
+    author="abel",
+    packages=['dev_aberto'],
+    python_requires='>=3.6',
+    platforms=['Linux', 'Windows']
+    )
--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(name='dev_aberto_abel',
-      version='0.1',
-      packages=['pacote_exemplo/dev_aberto']
-      )
